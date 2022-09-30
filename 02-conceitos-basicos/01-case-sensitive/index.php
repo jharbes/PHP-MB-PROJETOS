42ae57d3e420e40da534cf84509ffb8b0101db72
Jorge Harbes <jharbes@hotmail.com>
2022-09-30 15:59:59 -0300

    Create index.php

---
+++ 02-conceitos-basicos/01-case-sensitive/index.php
@@ -0,0 +1,15 @@
+<?php
+
+// não é case sensitive para instrucoes
+ECHO "teste sensitive<br>";
+ecHO "teste2";
+
+// é case sensitive para variaveis
+
+$nome="<br>Matheus";
+$NOME='<br>Joao';
+
+echo $nome;
+echo $NOME;
+
+?>
