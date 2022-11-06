8bf293bf0544f2a6d86a0ffbb9b7d292f247c966
Jorge Harbes <jharbes@hotmail.com>
2022-11-06 11:32:18 -0300

    Update estruturaSwitch.php

--- 06-estruturas-de-controle/estruturaSwitch.php
+++ 06-estruturas-de-controle/estruturaSwitch.php
@@ -20,14 +20,14 @@ $x = 5;
 
 switch($x) {
 
-case 0:
-    echo "X é igual a 0 <br>";
-    break;
-case 1:
-    echo "X é igual 1 <br>";
-    break;
-default:
-    echo "X não é igual a nenhum dos valores mencionados <br>";
+    case 0:
+        echo "X é igual a 0 <br>";
+        break;
+    case 1:
+        echo "X é igual 1 <br>";
+        break;
+    default:
+        echo "X não é igual a nenhum dos valores mencionados <br>";
 
 }
 
@@ -35,14 +35,14 @@ $y = "";
 
 switch($y) {
 
-case "Matheus":
-    echo "O nome é Matheus";
-    break;
-case "João":
-    echo "O nome é João";
-    break;
-default:
-    echo "O nome não foi encontrado";
+    case "Matheus":
+        echo "O nome é Matheus";
+        break;
+    case "João":
+        echo "O nome é João";
+        break;
+    default:
+        echo "O nome não foi encontrado";
     
 }
   
