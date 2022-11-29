9cf7fa9f36277365ea90a318a5b8013d59ccafe1
Jorge Harbes <jharbes@hotmail.com>
2022-11-29 14:39:54 -0300

    Update exercicio38.php

--- 09-funcoes/exercicio38.php
+++ 09-funcoes/exercicio38.php
@@ -25,7 +25,34 @@ function vetorEmString($vetor){
 }
 
 echo vetorEmString(["sabao","refrigerante","arroz","queijo","carne","suco"]);
-  
+
+
+
+/*
+*  SOLUCAO DO PROFESSOR
+*/
+
+$lista = ["Arroz", "Trigo", "Cerveja", "Sal", "Vinagre"];
+
+function listaParaString($arr) {
+
+  $str = "Você levou estes itens do mercado: ";
+
+  for($i = 0; $i < count($arr); $i++) {
+
+    if($i + 1 == count($arr)) {
+      $str .= "$arr[$i].";
+    } else {
+      $str .= "$arr[$i], ";
+    }
+
+  }
+
+  return $str;
+
+}
+
+echo listaParaString($lista);
 ?>
 </body>
 </html>
