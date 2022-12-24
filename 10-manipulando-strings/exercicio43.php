f22e3cf7b6ec69cbe526ec9a41eb97f2090ee179
Jorge Harbes <jharbes@hotmail.com>
2022-12-24 14:59:13 -0300

    Create exercicio43.php

---
+++ 10-manipulando-strings/exercicio43.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Estudos de PHP</title>
+    <style>
+        h3{
+            background-color: yellowgreen;
+            width: fit-content;
+        }
+        p{
+            border: 1px solid black;
+            width: fit-content;
+        }
+    </style>
+</head>
+<body>
+<h1>Curso de PHP</h1><hr>
+<?php
+  
+$string="carro - navio - helicóptero - barco - jangada";
+
+$stringArray=explode("-",$string);
+
+foreach ($stringArray as $arrayItem)
+    echo "$arrayItem<br>";
+  
+?>
+</body>
+</html>
